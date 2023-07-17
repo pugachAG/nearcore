48a3f022006262374ac7cc3dad7c3ef224380b23
Anton Puhach <anton@near.org>
2023-07-17 11:56:06 +0200

    impl

--- core/store/src/db/rocksdb.rs
+++ core/store/src/db/rocksdb.rs
@@ -476,7 +476,7 @@ fn rocksdb_block_based_options(
     block_opts
         .set_block_cache(&Cache::new_lru_cache(cache_size.as_u64().try_into().unwrap()).unwrap());
     block_opts.set_pin_l0_filter_and_index_blocks_in_cache(true);
-    block_opts.set_cache_index_and_filter_blocks(true);
+    block_opts.set_cache_index_and_filter_blocks(false);
     block_opts.set_bloom_filter(10.0, true);
     block_opts
 }
